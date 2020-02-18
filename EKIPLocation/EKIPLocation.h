cb7182a5d2bf7158a811cb50f3e50798f935fa2c
Emil Karimov <emvakar@gmail.com>
2020-02-18 14:44:25 +0300

    Initial Commit

---
+++ EKIPLocation/EKIPLocation.h
@@ -0,0 +1,19 @@
+//
+//  EKIPLocation.h
+//  EKIPLocation
+//
+//  Created by Emil Karimov on 18.02.2020.
+//  Copyright © 2020 Emil Karimov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EKIPLocation.
+FOUNDATION_EXPORT double EKIPLocationVersionNumber;
+
+//! Project version string for EKIPLocation.
+FOUNDATION_EXPORT const unsigned char EKIPLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EKIPLocation/PublicHeader.h>
+
+
